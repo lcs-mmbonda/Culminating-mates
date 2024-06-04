47cf46e36148d5cc015233ee40226b7b061eecd1
Myers <mmbonda@lcs.on.ca>
2024-06-04 09:44:49 -0400

    Initial Commit

---
+++ Culminating-mates/Culminating_matesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_matesApp.swift
+//  Culminating-mates
+//
+//  Created by Myers Elliott Mbonda on 2024-06-04.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_matesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

7ddf4e3e24b2dd49fca3895158b84de23c330e61
Myers <mmbonda@lcs.on.ca>
2024-06-06 11:18:09 -0400

    Done list

---
+++ Culminating-mates/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Culminating-mates
+//
+//  Created by Myers Elliott Mbonda on 2024-06-04.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ Culminating-mates/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  Culminating-mates
+//
+//  Created by Myers Elliott Mbonda on 2024-06-06.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}

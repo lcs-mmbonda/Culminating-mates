b1a5c9467441079b364b2ff4bfa030cf112b76b7
Myers <mmbonda@lcs.on.ca>
2024-06-11 08:58:24 -0400

    Added Genre picker as well as a few computed properties (will be changing genre picker to subject picker

--- Culminating-mates/Views/AddSessionView.swift
+++ Culminating-mates/Views/AddSessionView.swift
@@ -12,7 +12,7 @@ struct AddSessionView: View {
     // Mark: Stored properties
     @State private var title: String = ""
     @State private var author: String = ""
-    @State private var genre: String = ""
+    @State private var genre: Int = 0
     @State private var dateStarted:  Date = Date()
     @State private var dataFinished: Date = Date()
     @State private var starRating: Int = 0
@@ -21,9 +21,21 @@ struct AddSessionView: View {
     
     //Mark: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Book Details")) {
+                
+                TextField("Title", text: $title)
+                TextField("Author", text: $author)
+                Picker("Genre", selection: $genre){
+                    Text("Romance").tag(1)
+                    Text("Science Fiction").tag(2)
+                }
+                
+            }
+        }
     }
 }
+
 
 #Preview {
     AddSessionView()

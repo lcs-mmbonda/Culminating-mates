307be2639a01db7638ce3afaafaf7c33c8fc46a4
Myers <mmbonda@lcs.on.ca>
2024-06-11 08:51:34 -0400

    Added stored properties for addreviewView tabview even though I will be editing to make it applicable to my code which is a addSession view

---
+++ Culminating-mates/Views/AddSessionView.swift
@@ -0,0 +1,18 @@
+//
+//  AddSessionView.swift
+//  Culminating-mates
+//
+//  Created by Myers Elliott Mbonda on 2024-06-11.
+//
+
+import SwiftUI
+
+struct AddSessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddSessionView()
+}

6e46879cd177d588839e6110e13e1276aad3b69c
Myers <mmbonda@lcs.on.ca>
2024-06-11 08:52:01 -0400

    Added stored properties for addreviewView tabview even though I will be editing to make it applicable to my code which is a addSession view

--- Culminating-mates/Views/AddSessionView.swift
+++ Culminating-mates/Views/AddSessionView.swift
@@ -8,6 +8,18 @@
 import SwiftUI
 
 struct AddSessionView: View {
+    
+    // Mark: Stored properties
+    @State private var title: String = ""
+    @State private var author: String = ""
+    @State private var genre: String = ""
+    @State private var dateStarted:  Date = Date()
+    @State private var dataFinished: Date = Date()
+    @State private var starRating: Int = 0
+    @State private var review: String = ""
+    
+    
+    //Mark: Computed properties
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
